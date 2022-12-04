c23b149242c52cab44a59977245b5170a51c458e
Alsu <shakilsu@yandex.ru>
2022-12-04 14:53:17 +0300

    task66,68

--- task66/Program.cs
+++ task66/Program.cs
@@ -1,3 +1,15 @@
 ﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 //M = 1; N = 15 -> 120
 //M = 4; N = 8. -> 30
+
+int SumNumbers(int M, int N)
+{
+    if (M == N) return M;                      
+        else if (M < N) return N + SumNumbers(M, N - 1); 
+        else if (N == 0 && M == 0) return (M * (M + 1)) / 2;    
+        else return N + SumNumbers(M, N + 1);      
+}
+
+int M = 1;
+int N = 15;
+Console.WriteLine($"Сумма натуральных элементов в промежутке от M({M}) до N({N}) = {SumNumbers(M, N)}");;
--- task68/Program.cs
+++ task68/Program.cs
@@ -1,3 +1,16 @@
 ﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 //m = 2, n = 3 -> A(m,n) = 9
 //m = 3, n = 2 -> A(m,n) = 29
+
+int AckermanFunction (int m, int n)
+{
+if (m == 0) return n + 1;
+if (m != 0 && n == 0) return AckermanFunction(m - 1, 1);
+if (m > 0 && n > 0) return AckermanFunction(m - 1, AckermanFunction(m, n - 1));
+return AckermanFunction(m, n);
+}
+
+
+int m = 3;
+int n = 2;
+Console.WriteLine($"Функция Аккермана для чисел m,n: A({m},{n}) = {AckermanFunction(m, n)}");
